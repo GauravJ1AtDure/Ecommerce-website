85fce981527dee85268fdb080000efebbcaac143
git config --global user.name GauravJ1AtDure <92713528+GauravJ1AtDure@users.noreply.github.com>
2024-03-11 17:44:40 +0530

    working on home page and adding extra products

--- script/home.js
+++ script/home.js
@@ -1,3 +1,6 @@
+let productArr=[]
+let homePageCards=document.querySelector('#homePageCards')
+let selected_product
 
 fetch("/data/products.json")
 .then(response => {
@@ -5,23 +8,21 @@ fetch("/data/products.json")
 })
 .then(data => { 
     //console.log(data[0].products_101)
-   
+    productArr = [...data[0].products_101]
     
-    let homePageCards=document.querySelector('#homePageCards')
-    let selected_product
-    let checkout=(x)=>{
-      console.log('card click',x)
-      selected_product=data[0].products_101[x-1]
-      localStorage.setItem('selectedProduct',JSON.stringify(selected_product))
-      location.href='/pages/product_card.html'
-    }
-  
-    homePageCards.addEventListener('click',checkout)
 
 
 });
 
 
-
+    
+    let checkout=(x)=>{
+      console.log('card click',x)
+      selected_product=productArr[x-1]
+      localStorage.setItem('selectedProduct',JSON.stringify(selected_product))
+      location.href='/pages/product_card.html'
+    }
+  
+    //homePageCards.addEventListener('click',checkout)
 
  
